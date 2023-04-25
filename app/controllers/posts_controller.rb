fed57b1cf5ccfd5c2aa8dfded5b9c354264c28d8
LR <lr177245@gmail.com>
2023-04-25 14:31:14 -0700

    can crud posts; unlinked from user for now

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,55 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+    render json: @posts
+  end
+
+  def show
+    render json: @post
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.status = 'draft'
+    if @post.save
+      render json: @post.body.length, status: :created
+    else
+      render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if params[:body].empty?
+      render json: "Can't update without a body!"
+    elsif !@post
+      render json: "Couldn't find post"
+    else
+      @post.update(post_params)
+      render json: @post
+    end
+  end
+
+  def destroy
+    if !@post
+      render json: "Couldn't find post"
+    else
+      @id = @post.id
+      if @post.destroy
+        render json: "Deleted post #{@id}"
+      else
+        render json: "Couldn't delete post #{@id}"
+      end
+    end
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.permit(:body, :status)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,10 @@
 ActiveRecord::Schema.define(version: 2023_04_25_193755) do
 
   create_table "posts", force: :cascade do |t|
-    t.text "body"
-    t.string "status"
-    t.integer "user_id", null: false
+    t.text "body", null: false
+    t.string "status", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,5 +25,4 @@ ActiveRecord::Schema.define(version: 2023_04_25_193755) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "posts", "users"
 end
