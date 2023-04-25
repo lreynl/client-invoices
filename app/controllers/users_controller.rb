2998715e01ad83df2eb845e841781ca564a005fb
LR <lr177245@gmail.com>
2023-04-25 13:06:44 -0700

    can create user and list all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.all
+    render json: @user
+  end
+
+  def show
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    render json: @user, status: :created
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.permit(:name)
+  end
 end
