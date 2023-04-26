336f3973a848d7f6daaa567192f602de19aa0df5
LR <lr177245@gmail.com>
2023-04-25 21:41:14 -0700

    set status default to 0

--- db/migrate/20230425193755_create_posts.rb
+++ db/migrate/20230425193755_create_posts.rb
@@ -2,9 +2,9 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.text :body, null: false
-      t.string :status, null: false
+      t.string :status, default: 0, null: false
       t.string :uuid, null: false
-      t.string :file_link, null: false
+      t.string :file_url, null: false
       # t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2023_04_26_011137) do
     t.text "body", null: false
     t.string "status", null: false
     t.string "uuid", null: false
+    t.string "file_url", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
